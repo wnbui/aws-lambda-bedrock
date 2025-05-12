5f51f82bdb2ba059a0bc83d7081e5e289ea2f439
wnbui <wnbui05@gmail.com>
2025-05-11 22:57:43 -0700

    Update script

--- package_lambda.sh
+++ package_lambda.sh
@@ -5,6 +5,6 @@ echo "Packaging lambda_function.py -> lambda_function.zip..."
 
 rm -f lambda_function.zip
 
-zip lambda_function.py lambda_function.py
+zip lambda_function.zip lambda_function.py
 
 echo "Created lambda_function.zip"
